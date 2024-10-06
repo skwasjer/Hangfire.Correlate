5169e2a8fc69501e363e24e678879a4ffe07090a
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-10-06 09:17:29 +0200

    test: mark tests also as obsolete, so STA does not complain

--- test/Hangfire.Correlate.Tests/HangfireBuiltInConfigurationTests.cs
+++ test/Hangfire.Correlate.Tests/HangfireBuiltInConfigurationTests.cs
@@ -4,17 +4,20 @@ using Hangfire.MemoryStorage;
 using Hangfire.Server;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace Hangfire.Correlate;
 
 /// <summary>
-/// Tests Hangfire integration with the <see cref="GlobalConfigurationExtensions.UseCorrelate(IGlobalConfiguration,ILoggerFactory)" /> overload.
+/// Tests Hangfire integration with the <see cref="GlobalConfigurationExtensions.UseCorrelate(IGlobalConfiguration,ILoggerFactory)" />
+/// overload.
 /// </summary>
 /// <remarks>
-/// Parallel test execution is not supported since we use memory storage with Hangfire that is being set into a static property Storage.Current. When tests are run in parallel, the test that last set the storage will win, while the others will break. This is also true for other Hangfire dependencies, but they do not directly affect our tests atm.
+/// Parallel test execution is not supported since we use memory storage with Hangfire that is being set into a static property
+/// Storage.Current. When tests are run in parallel, the test that last set the storage will win, while the others will break. This is also
+/// true for other Hangfire dependencies, but they do not directly affect our tests atm.
 /// </remarks>
+[Obsolete("Use the overload accepting an IServiceProvider.")]
 public class HangfireBuiltInConfigurationTests : HangfireIntegrationTests
 {
     private readonly LoggerFactory _loggerFactory;
@@ -26,13 +29,14 @@ public class HangfireBuiltInConfigurationTests : HangfireIntegrationTests
         GlobalConfiguration.Configuration
             .UseCorrelate(_loggerFactory)
             .UseActivator(
-                new BackgroundTestExecutorJobActivator(() =>
-                    new BackgroundTestExecutor(
-                        CreateTestService(),
-                        new CorrelationContextAccessor(),
-                        ExecutedJobs,
-                        testOutputHelper
-                    )
+                new BackgroundTestExecutorJobActivator(
+                    () =>
+                        new BackgroundTestExecutor(
+                            CreateTestService(),
+                            new CorrelationContextAccessor(),
+                            ExecutedJobs,
+                            testOutputHelper
+                        )
                 )
             )
             .UseMemoryStorage();
@@ -43,9 +47,15 @@ public class HangfireBuiltInConfigurationTests : HangfireIntegrationTests
         var correlatingHttpMessageHandler = new CorrelatingHttpMessageHandler(
             new CorrelationContextAccessor(),
             Options.Create(new CorrelateClientOptions())
-        ) { InnerHandler = MockHttp };
+        )
+        {
+            InnerHandler = MockHttp
+        };
         return new TestService(
-            new HttpClient(correlatingHttpMessageHandler, true) { BaseAddress = new Uri("http://0.0.0.0") }
+            new HttpClient(correlatingHttpMessageHandler, true)
+            {
+                BaseAddress = new Uri("http://0.0.0.0")
+            }
         );
     }
 
